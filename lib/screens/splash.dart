811ef3ff08ad0f6d2d99f711030b64edda6a4051
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-09 21:43:51 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -4,7 +4,23 @@ class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return SafeArea(
-      child: Scaffold(),
+      child: Scaffold(
+        body: Container(
+          color: mainTextColor,
+          child: Center(
+            child: Text(
+              'MECL',
+              style: TextStyle(
+                fontFamily: 'Bambino',
+                letterSpacing: 0.5,
+                fontSize: ScreenUtil().setSp(90),
+                color: mainBgColor,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
