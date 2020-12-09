f76a345704de13ac34e6bdca31083565b129b06d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-09 21:41:15 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -3,6 +3,8 @@ import 'package:flutter/material.dart';
 class SplashScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return SafeArea(
+      child: Scaffold(),
+    );
   }
 }
