ce0fa13c50558ada7711d3811197a597de03754f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 21:55:08 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:google_fonts/google_fonts.dart';
 import 'package:payup/utilities/constants.dart';
 
 class SplashScreen extends StatelessWidget {
