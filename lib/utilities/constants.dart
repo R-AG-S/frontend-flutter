71cd841af04255f411f6cdc88c171b5692a797c9
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 22:07:19 +0530

    app update

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -3,4 +3,6 @@ import 'package:flutter/material.dart';
 final Color mainBgColor = Color(0xFFF19F1E);
 final Color whiteColor = Color(0xFFFFFFFF);
 final Color mainTextColor = Color(0xFF000000);
-final Color fadeTextColor = Color(0xFF000000);
+final Color fadeTextColor = Color(0xFF8B8A8F);
+final Color darkFadeTextColor = Color(0xFF60606A);
+final Color lightFadeText = Colors.white70;
