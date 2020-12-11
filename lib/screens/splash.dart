4bcf82ce8f90ab6e7f78499fe989885acddb8eb7
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 21:22:40 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -13,8 +13,11 @@ class SplashScreen extends StatelessWidget {
             child: Column(
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
-                Image(
-                  image: AssetImage('images/icon.png'),
+                Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Image(
+                    image: AssetImage('images/icon.png'),
+                  ),
                 ),
                 Text(
                   'PayUp',
