384c52498cb48027347cd92b351cb3ff57edcfab
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 22:22:48 +0530

    app update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:payup/screens/register.dart';
 import 'package:payup/utilities/constants.dart';
+import 'package:sign_button/create_button.dart';
 
 class LoginScreen extends StatefulWidget {
   @override
