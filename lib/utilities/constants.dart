84f5b590e640e3a899268dd26757ff452d8bee20
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 22:19:44 +0530

    app update

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -6,3 +6,4 @@ final Color mainTextColor = Color(0xFF000000);
 final Color fadeTextColor = Color(0xFF8B8A8F);
 final Color darkFadeTextColor = Color(0xFF60606A);
 final Color lightFadeText = Colors.white70;
+final Color redColor = Color(0xFFFE2C55);
