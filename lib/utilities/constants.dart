9ff6329f651841831da19adff1d890f20ac35335
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-11 22:05:53 +0530

    app update

--- lib/utilities/constants.dart
+++ lib/utilities/constants.dart
@@ -2,4 +2,4 @@ import 'package:flutter/material.dart';
 
 final Color mainBgColor = Color(0xFFF19F1E);
 final Color whiteColor = Color(0xFFFFFFFF);
-final Color mainBgColor = Color(0xFF000000);
+final Color mainTextColor = Color(0xFF000000);
