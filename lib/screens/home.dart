739fbb7812657649f59f9da5bba91389c110abcc
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-12 20:56:57 +0530

    app update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 
 class HomeScreen extends StatefulWidget {
   @override
@@ -52,7 +53,7 @@ class _HomeScreenState extends State<HomeScreen> {
       onTap: () => _showSnackBar('Delete'),
     ),
   ],
-);
+),
     );
   }
 }
