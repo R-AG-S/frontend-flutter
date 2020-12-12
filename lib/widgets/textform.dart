62677f952e75b2eb0d2c6b56ebd7db8e195c88bd
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-12 17:55:49 +0530

    app update

---
+++ lib/widgets/textform.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:library_mec/utilities/constants.dart';
+
+class TextFormWidget extends StatelessWidget {
+  const TextFormWidget({
+    @required this.firstNameController,
+    @required this.type,
+    @required this.obscure,
+  });
+  final TextEditingController firstNameController;
+  final TextInputType type;
+  final bool obscure;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      style: TextStyle(color: mainTextColor),
+      cursorColor: mainTextColor,
+      controller: firstNameController,
+      keyboardType: type,
+      maxLength: 50,
+      obscureText: obscure,
+      decoration: InputDecoration(
+        border: OutlineInputBorder(
+          borderSide: BorderSide.none,
+          borderRadius: BorderRadius.all(
+            Radius.circular(5.0),
+          ),
+        ),
+        contentPadding: EdgeInsets.symmetric(
+          vertical: 10,
+          horizontal: 15,
+        ),
+        filled: true,
+        fillColor: cardBgColor,
+        counterText: '',
+      ),
+    );
+  }
+}
