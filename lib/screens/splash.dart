96530b055ac577b1041cff02bf08d7b60a3c9ca1
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-12 18:12:21 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:payup/utilities/constants.dart';
 
-class SplashScreen extends StatelessWidget {
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
