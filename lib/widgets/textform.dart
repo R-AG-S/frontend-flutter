ddba2e97f2a151f0cb23e2b0fb25bb2f2ad45c7d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-12 17:56:41 +0530

    app update

--- lib/widgets/textform.dart
+++ lib/widgets/textform.dart
@@ -32,7 +32,7 @@ class TextFormWidget extends StatelessWidget {
           horizontal: 15,
         ),
         filled: true,
-        fillColor: cardBgColor,
+        fillColor: Color(0xFF202225),
         counterText: '',
       ),
     );
