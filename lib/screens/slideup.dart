2e8829624ba618bd4f56c415bbbbc949b2ac4934
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 12:02:08 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -8,14 +8,27 @@ class SlideUpPanel extends StatefulWidget {
   _SlideUpPanelState createState() => _SlideUpPanelState();
 }
 
+double _panelHeightOpen;
+double _panelHeightClosed;
+BorderRadiusGeometry radius = BorderRadius.only(
+  topLeft: Radius.circular(24.0),
+  topRight: Radius.circular(24.0),
+);
+
 class _SlideUpPanelState extends State<SlideUpPanel> {
   @override
   Widget build(BuildContext context) {
+    _panelHeightOpen = MediaQuery.of(context).size.height * 0.8;
+    _panelHeightClosed = MediaQuery.of(context).size.height * 0.1;
     return Scaffold(
       appBar: AppBar(
         title: Text("SlidingUpPanelExample"),
       ),
       body: SlidingUpPanel(
+        backdropEnabled: true,
+        borderRadius: radius,
+        maxHeight: _panelHeightOpen,
+        minHeight: _panelHeightClosed,
         collapsed: Center(
           child: FaIcon(
             FontAwesomeIcons.chevronUp,
