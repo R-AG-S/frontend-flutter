c36a48f21d974953488e18736cfa3cce8657d25c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 21:16:51 +0530

    app update

--- lib/utilities/data.dart
+++ lib/utilities/data.dart
@@ -1,1 +1,3 @@
-final List iconData = ['https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F01.png?alt=media&token=23632865-e803-4715-957d-8ba414809d0a',]
+final List iconData = [
+  'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F01.png?alt=media&token=23632865-e803-4715-957d-8ba414809d0a',
+];
