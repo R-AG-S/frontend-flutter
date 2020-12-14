aee0f6f694a60607cb9eefa6b572c6bd8919f75b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 18:54:12 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:gradient_widgets/gradient_widgets.dart';
 import 'package:payup/utilities/constants.dart';
 
 class SlideUpPanel extends StatefulWidget {
