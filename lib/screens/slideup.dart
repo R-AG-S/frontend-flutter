67a7e875ee23252d8edb9a4016be04326afa5b90
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 11:52:28 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -1,1 +1,13 @@
+import 'package:flutter/material.dart';
 
+class SlideUpPanel extends StatefulWidget {
+  @override
+  _SlideUpPanelState createState() => _SlideUpPanelState();
+}
+
+class _SlideUpPanelState extends State<SlideUpPanel> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
