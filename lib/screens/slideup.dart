d97730103beba038ed4046a068d0fa3e0dd735b5
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 11:52:35 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -6,8 +6,19 @@ class SlideUpPanel extends StatefulWidget {
 }
 
 class _SlideUpPanelState extends State<SlideUpPanel> {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
-  }
+ @override
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: Text("SlidingUpPanelExample"),
+    ),
+    body: SlidingUpPanel(
+      panel: Center(
+        child: Text("This is the sliding Widget"),
+      ),
+      body: Center(
+        child: Text("This is the Widget behind the sliding panel"),
+      ),
+    ),
+  );
 }
