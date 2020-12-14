7a746f50e39a70bba5e25ce2a509338f2285ec17
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 21:32:27 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -199,6 +199,7 @@ class _SlideUpPanelState extends State<SlideUpPanel> {
                         return Column(
                           children: [
                             Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
                               children: [
                                 FadeInImage.assetNetwork(
                                   fadeOutDuration: Duration(seconds: 1),
@@ -207,7 +208,14 @@ class _SlideUpPanelState extends State<SlideUpPanel> {
                                   height: 50,
                                   width: 50,
                                 ),
-                                Text('user $index')
+                                Text(
+                                  'Demo User $index',
+                                  style: GoogleFonts.openSans(
+                                    fontSize: 14,
+                                    fontWeight: FontWeight.w400,
+                                    color: darkFadeTextColor,
+                                  ),
+                                ),
                               ],
                             ),
                             Divider()
@@ -269,5 +277,32 @@ class CircularKeys extends StatelessWidget {
         ],
       ),
     );
+  }
+}
+
+class CircleTabIndicator extends Decoration {
+  final BoxPainter _painter;
+
+  CircleTabIndicator({@required Color color, @required double radius})
+      : _painter = _CirclePainter(color, radius);
+
+  @override
+  BoxPainter createBoxPainter([onChanged]) => _painter;
+}
+
+class _CirclePainter extends BoxPainter {
+  final Paint _paint;
+  final double radius;
+
+  _CirclePainter(Color color, this.radius)
+      : _paint = Paint()
+          ..color = color
+          ..isAntiAlias = true;
+
+  @override
+  void paint(Canvas canvas, Offset offset, ImageConfiguration cfg) {
+    final Offset circleOffset =
+        offset + Offset(cfg.size.width / 2, cfg.size.height - radius);
+    canvas.drawCircle(circleOffset, radius, _paint);
   }
 }
