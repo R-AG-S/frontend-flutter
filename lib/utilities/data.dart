c30a955bbdef3a9d06389162bd9d5bccd42436ae
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 21:16:42 +0530

    app update

--- lib/utilities/data.dart
+++ lib/utilities/data.dart
@@ -1,1 +1,1 @@
-import 'package:flutter/material.dart';
+final List iconData = ['https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F01.png?alt=media&token=23632865-e803-4715-957d-8ba414809d0a',]
