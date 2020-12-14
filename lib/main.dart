97fa24ff7afda73f8e7dfc427bd0587df9a08f77
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 11:54:15 +0530

    app update

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,7 @@ class _MainAppState extends State<MainApp> {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'splash',
+      initialRoute: 'slide',
       routes: {
         'splash': (context) => SplashScreen(),
         'login': (context) => LoginScreen(),
