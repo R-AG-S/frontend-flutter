40287b6f0b3b23b9203ae3760183ef6036f9b8a6
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-14 21:18:15 +0530

    app update

--- lib/utilities/data.dart
+++ lib/utilities/data.dart
@@ -1,3 +1,7 @@
 final List iconData = [
   'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F01.png?alt=media&token=23632865-e803-4715-957d-8ba414809d0a',
+  'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F02.png?alt=media&token=7f7dfeb2-c002-49cd-ad47-9547feaf4578',
+  'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F03.png?alt=media&token=b80264d8-7733-46cb-84d2-2e26bf16e258',
+  'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F04.png?alt=media&token=dab36439-890f-44e6-9845-b413ccaf4528',
+  'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F05.png?alt=media&token=9d1416f0-5bb2-4f8a-9b20-690ce6efc4a4',
 ];
