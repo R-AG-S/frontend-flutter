dd2aa224a4cc48e684f0e7e2e901dc66b83beff2
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-15 16:22:00 +0530

    app update

--- lib/screens/userSettings.dart
+++ lib/screens/userSettings.dart
@@ -23,88 +23,90 @@ class _UserSettingsState extends State<UserSettings> {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: Stack(
-        children: [
-          SingleChildScrollView(
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              children: [
-                Center(
-                  child: GestureDetector(
-                    onTap: () {
-                      // _showPicker(context);
-                    },
-                    child: Padding(
-                      padding: EdgeInsets.only(top: 25),
-                      child: ClipRRect(
-                        borderRadius: BorderRadius.circular(50),
-                        child: Image.network(
-                          'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F63.png?alt=media&token=bbd3fdc4-f800-4ee9-ac58-173a395dbfa8',
-                          width: 100,
-                          height: 100,
-                          fit: BoxFit.fitHeight,
+      home: Scaffold(
+        body: Stack(
+          children: [
+            SingleChildScrollView(
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  Center(
+                    child: GestureDetector(
+                      onTap: () {
+                        // _showPicker(context);
+                      },
+                      child: Padding(
+                        padding: EdgeInsets.only(top: 25),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.network(
+                            'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F63.png?alt=media&token=bbd3fdc4-f800-4ee9-ac58-173a395dbfa8',
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.fitHeight,
+                          ),
                         ),
+                        // child: _image != null
+                        //     ? ClipRRect(
+                        //         borderRadius: BorderRadius.circular(50),
+                        //         child: Image.file(
+                        //           _image,
+                        //           width: 100,
+                        //           height: 100,
+                        //           fit: BoxFit.fitHeight,
+                        //         ),
+                        //       )
+                        //     : userImage == null
+                        //         ? Container(
+                        //             decoration: BoxDecoration(
+                        //                 color: Colors.grey[200],
+                        //                 borderRadius: BorderRadius.circular(50)),
+                        //             width: 100,
+                        //             height: 100,
+                        //             child: Icon(
+                        //               Icons.camera_alt,
+                        //               color: Colors.grey[800],
+                        //             ),
+                        //           )
+                        //         : ClipRRect(
+                        //             borderRadius: BorderRadius.circular(50),
+                        //             child: Image.network(
+                        //               userImage,
+                        //               width: 100,
+                        //               height: 100,
+                        //               fit: BoxFit.fitHeight,
+                        //             ),
+                        //           ),
                       ),
-                      // child: _image != null
-                      //     ? ClipRRect(
-                      //         borderRadius: BorderRadius.circular(50),
-                      //         child: Image.file(
-                      //           _image,
-                      //           width: 100,
-                      //           height: 100,
-                      //           fit: BoxFit.fitHeight,
-                      //         ),
-                      //       )
-                      //     : userImage == null
-                      //         ? Container(
-                      //             decoration: BoxDecoration(
-                      //                 color: Colors.grey[200],
-                      //                 borderRadius: BorderRadius.circular(50)),
-                      //             width: 100,
-                      //             height: 100,
-                      //             child: Icon(
-                      //               Icons.camera_alt,
-                      //               color: Colors.grey[800],
-                      //             ),
-                      //           )
-                      //         : ClipRRect(
-                      //             borderRadius: BorderRadius.circular(50),
-                      //             child: Image.network(
-                      //               userImage,
-                      //               width: 100,
-                      //               height: 100,
-                      //               fit: BoxFit.fitHeight,
-                      //             ),
-                      //           ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: EdgeInsets.only(top: 10),
-                  child: Text(
-                    "Change Profile Pic",
-                    style: TextStyle(
-                      color: darkFadeTextColor,
-                      fontSize: 18,
-                      fontWeight: FontWeight.w600,
+                  Padding(
+                    padding: EdgeInsets.only(top: 10),
+                    child: Text(
+                      "Change Profile Pic",
+                      style: TextStyle(
+                        color: darkFadeTextColor,
+                        fontSize: 18,
+                        fontWeight: FontWeight.w600,
+                      ),
+                      textAlign: TextAlign.center,
                     ),
-                    textAlign: TextAlign.center,
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Padding(
-            padding: EdgeInsets.only(
-                top: MediaQuery.of(context).size.height * 0.250),
-            child: Tabbar(
-              userId: 'userID',
-              userMail: 'user@gmail.com',
-              userName: 'User Name',
-              userNumber: 91233445654,
-            ),
-          )
-        ],
+            Padding(
+              padding: EdgeInsets.only(
+                  top: MediaQuery.of(context).size.height * 0.250),
+              child: Tabbar(
+                userId: 'userID',
+                userMail: 'user@gmail.com',
+                userName: 'User Name',
+                userNumber: 91233445654,
+              ),
+            )
+          ],
+        ),
       ),
     );
   }
