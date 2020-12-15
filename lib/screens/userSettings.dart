9d6bc1ee3784b396696831c8238a825267514d0f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-15 15:48:07 +0530

    app update

--- lib/screens/userSettings.dart
+++ lib/screens/userSettings.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class UserSettings extends StatefulWidget {
+  @override
+  _UserSettingsState createState() => _UserSettingsState();
+}
+
+class _UserSettingsState extends State<UserSettings> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
