21ca160704f89f6a6bb5e70dd04308d0b437fa76
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-15 22:42:00 +0530

    app update

--- lib/screens/userSettings.dart
+++ lib/screens/userSettings.dart
@@ -162,63 +162,26 @@ class _UserSettingsState extends State<UserSettings> {
                   size: 18,
                 ),
               ),
-              SettingsList(
-                shrinkWrap: true,
-                sections: [
-                  SettingsSection(
-                    title: 'General',
-                    titleTextStyle: TextStyle(
-                      fontFamily: 'Bambino',
-                      fontSize: ScreenUtil().setSp(40),
-                      color: whiteColor,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    tiles: [
-                      SettingsTile(
-                        title: 'Edit Listed Cars',
-                        titleTextStyle: GoogleFonts.openSans(
-                          fontSize: ScreenUtil().setSp(45),
-                          fontWeight: FontWeight.w600,
-                          color: whiteColor,
-                        ),
-                        leading: Icon(
-                          FontAwesomeIcons.carAlt,
-                          size: 18,
-                        ),
-                      ),
-                    ],
-                  ),
-                  SettingsSection(
-                    title: 'Misc',
-                    titleTextStyle: TextStyle(
-                      fontFamily: 'Bambino',
-                      fontSize: ScreenUtil().setSp(40),
-                      color: whiteColor,
-                      fontWeight: FontWeight.w400,
-                    ),
-                    tiles: [
-                      SettingsTile(
-                          title: 'Terms of Service',
-                          titleTextStyle: GoogleFonts.openSans(
-                            fontSize: ScreenUtil().setSp(45),
-                            fontWeight: FontWeight.w600,
-                            color: whiteColor,
-                          ),
-                          leading: Icon(Icons.description)),
-                      SettingsTile(
-                        title: 'About',
-                        titleTextStyle: GoogleFonts.openSans(
-                          fontSize: ScreenUtil().setSp(45),
-                          fontWeight: FontWeight.w600,
-                          color: whiteColor,
-                        ),
-                        leading: Icon(
-                          Icons.info_outline_rounded,
-                        ),
-                      ),
-                    ],
-                  ),
-                ],
+              SettingsTile(
+                title: 'General',
+                titleTextStyle: TextStyle(
+                  fontFamily: 'Bambino',
+                  fontSize: ScreenUtil().setSp(40),
+                  color: whiteColor,
+                  fontWeight: FontWeight.w400,
+                ),
+              ),
+              SettingsTile(
+                title: 'Edit Listed Cars',
+                titleTextStyle: GoogleFonts.openSans(
+                  fontSize: ScreenUtil().setSp(45),
+                  fontWeight: FontWeight.w600,
+                  color: whiteColor,
+                ),
+                leading: Icon(
+                  FontAwesomeIcons.carAlt,
+                  size: 18,
+                ),
               ),
             ],
           ),
