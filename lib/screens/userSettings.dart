672db254e8128c6ac30e28ea45378127f3a52b96
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-15 15:58:18 +0530

    app update

--- lib/screens/userSettings.dart
+++ lib/screens/userSettings.dart
@@ -22,7 +22,80 @@ class _UserSettingsState extends State<UserSettings> {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
-      home: Container(),
+      home: Stack(
+        children: [
+          SingleChildScrollView(
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Center(
+                  child: GestureDetector(
+                    onTap: () {
+                      _showPicker(context);
+                    },
+                    child: Padding(
+                      padding: EdgeInsets.only(top: 25),
+                      child: _image != null
+                          ? ClipRRect(
+                              borderRadius: BorderRadius.circular(50),
+                              child: Image.file(
+                                _image,
+                                width: 100,
+                                height: 100,
+                                fit: BoxFit.fitHeight,
+                              ),
+                            )
+                          : userImage == null
+                              ? Container(
+                                  decoration: BoxDecoration(
+                                      color: Colors.grey[200],
+                                      borderRadius: BorderRadius.circular(50)),
+                                  width: 100,
+                                  height: 100,
+                                  child: Icon(
+                                    Icons.camera_alt,
+                                    color: Colors.grey[800],
+                                  ),
+                                )
+                              : ClipRRect(
+                                  borderRadius: BorderRadius.circular(50),
+                                  child: Image.network(
+                                    userImage,
+                                    width: 100,
+                                    height: 100,
+                                    fit: BoxFit.fitHeight,
+                                  ),
+                                ),
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.only(top: 10),
+                  child: Text(
+                    "Change Profile Pic",
+                    style: TextStyle(
+                      color: darkFadeTextColor,
+                      fontSize: 18,
+                      fontWeight: FontWeight.w600,
+                    ),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Padding(
+            padding: EdgeInsets.only(
+                top: MediaQuery.of(context).size.height * 0.250),
+            child: Tabbar(
+              userNumber: userNumber,
+              userMail: userMail,
+              userName: userName,
+              userId: userId,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
