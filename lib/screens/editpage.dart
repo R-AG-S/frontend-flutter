94e3bfaa65277cb0ab0ed84b94416a23cc130ed8
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 19:28:26 +0530

    app update

--- lib/screens/editpage.dart
+++ lib/screens/editpage.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:payup/utilities/constants.dart';
 
@@ -26,56 +27,72 @@ class _EditPageScreenState extends State<EditPageScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(
-            'Edit ' + widget.name,
-            style: TextStyle(
-              fontFamily: 'Bambino',
-              letterSpacing: 0.5,
-              fontSize: 22,
-              fontWeight: FontWeight.w400,
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.green,
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: darkFadeTextColor),
+        primaryTextTheme: Typography.material2018().black,
+        textTheme: Typography.material2018().black,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        primaryTextTheme: Typography.material2018().white,
+        textTheme: Typography.material2018().white,
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(
+              'Edit ' + widget.name,
+              style: TextStyle(
+                fontFamily: 'Bambino',
+                letterSpacing: 0.5,
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
+              ),
             ),
+            backgroundColor: Colors.transparent,
           ),
-          backgroundColor: Colors.transparent,
-        ),
-        body: Container(
-          padding: EdgeInsets.symmetric(
-            horizontal: 25,
-          ),
-          child: Column(
-            children: [
-              Padding(
-                padding: EdgeInsets.symmetric(
-                  vertical: 8,
-                ),
-                child: TextField(
-                  style: TextStyle(color: mainTextColor),
-                  cursorColor: mainTextColor,
-                  controller: _editController,
-                  keyboardType: widget.inputType,
-                  maxLength: 50,
-                  decoration: InputDecoration(
-                    labelText: widget.value,
-                    border: OutlineInputBorder(
-                      borderSide: BorderSide.none,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(5.0),
+          body: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Column(
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 8,
+                  ),
+                  child: TextField(
+                    style: TextStyle(color: mainTextColor),
+                    cursorColor: mainTextColor,
+                    controller: _editController,
+                    keyboardType: widget.inputType,
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                      labelText: widget.value,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
                       ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      filled: true,
+                      fillColor: cardColor,
+                      counterText: '',
                     ),
-                    contentPadding: EdgeInsets.symmetric(
-                      vertical: 10,
-                      horizontal: 15,
-                    ),
-                    filled: true,
-                    fillColor: cardColor,
-                    counterText: '',
                   ),
                 ),
-              ),
-            ],
+              ],
+            ),
           ),
         ),
       ),
