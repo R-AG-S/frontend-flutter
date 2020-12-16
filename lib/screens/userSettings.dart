fe2ba30e9add437eb7fb73ffd09684ccda1b8927
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 13:26:28 +0530

    app update

--- lib/screens/userSettings.dart
+++ lib/screens/userSettings.dart
@@ -32,159 +32,170 @@ class _UserSettingsState extends State<UserSettings> {
         primaryTextTheme: Typography.material2018().white,
         textTheme: Typography.material2018().white,
       ),
-      home: Scaffold(
-        extendBodyBehindAppBar: true,
-        appBar: AppBar(
-          elevation: 0,
-          title: Text(
-            'User Settings',
-            style: TextStyle(
-              fontFamily: 'Bambino',
-              letterSpacing: 0.5,
-              fontSize: 22,
-              fontWeight: FontWeight.w400,
-            ),
+      home: UserSettingScreen(),
+    );
+  }
+}
+
+class UserSettingScreen extends StatelessWidget {
+  const UserSettingScreen({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      extendBodyBehindAppBar: true,
+      appBar: AppBar(
+        elevation: 0,
+        title: Text(
+          'User Settings',
+          style: TextStyle(
+            fontFamily: 'Bambino',
+            letterSpacing: 0.5,
+            fontSize: 22,
+            fontWeight: FontWeight.w400,
           ),
-          backgroundColor: Colors.transparent,
         ),
-        body: SingleChildScrollView(
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            children: [
-              SizedBox(
-                height: 15,
-              ),
-              SettingsList(
-                shrinkWrap: true,
-                sections: [
-                  SettingsSection(
-                    title: 'Account Settings',
-                    titleTextStyle: TextStyle(
-                      fontFamily: 'Bambino',
-                      fontSize: ScreenUtil().setSp(40),
-                      fontWeight: FontWeight.w400,
-                    ),
-                    tiles: [],
+        backgroundColor: Colors.transparent,
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            SizedBox(
+              height: 15,
+            ),
+            SettingsList(
+              shrinkWrap: true,
+              sections: [
+                SettingsSection(
+                  title: 'Account Settings',
+                  titleTextStyle: TextStyle(
+                    fontFamily: 'Bambino',
+                    fontSize: ScreenUtil().setSp(40),
+                    fontWeight: FontWeight.w400,
                   ),
-                ],
-              ),
-              GestureDetector(
-                onTap: () {
-                  // _showPicker(context);
-                },
-                child: Padding(
-                  padding: EdgeInsets.only(top: 15),
-                  child: GestureDetector(
-                    onTap: () => _showPicker(context),
-                    child: Stack(
-                      children: [
-                        Container(
-                          decoration: BoxDecoration(
-                            shape: BoxShape.circle,
-                            color: whiteColor,
-                          ),
-                          padding: EdgeInsets.all(10),
-                          child: ClipRRect(
-                            borderRadius: BorderRadius.circular(50),
-                            child: Image.network(
-                              'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F63.png?alt=media&token=bbd3fdc4-f800-4ee9-ac58-173a395dbfa8',
-                              width: 100,
-                              height: 100,
-                              fit: BoxFit.fitHeight,
-                            ),
+                  tiles: [],
+                ),
+              ],
+            ),
+            GestureDetector(
+              onTap: () {
+                // _showPicker(context);
+              },
+              child: Padding(
+                padding: EdgeInsets.only(top: 15),
+                child: GestureDetector(
+                  onTap: () => _showPicker(context),
+                  child: Stack(
+                    children: [
+                      Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          color: whiteColor,
+                        ),
+                        padding: EdgeInsets.all(10),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(50),
+                          child: Image.network(
+                            'https://firebasestorage.googleapis.com/v0/b/inout-776aa.appspot.com/o/avatar-png%2F63.png?alt=media&token=bbd3fdc4-f800-4ee9-ac58-173a395dbfa8',
+                            width: 100,
+                            height: 100,
+                            fit: BoxFit.fitHeight,
                           ),
                         ),
-                        Padding(
-                          padding: EdgeInsets.only(
-                            top: 80,
-                            left: 80,
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(
+                          top: 80,
+                          left: 80,
+                        ),
+                        child: Container(
+                          decoration: BoxDecoration(
+                              shape: BoxShape.circle,
+                              color: mainTextColor,
+                              border: Border.all(
+                                color: whiteColor,
+                                width: 2,
+                              )),
+                          padding: EdgeInsets.all(
+                            10,
                           ),
-                          child: Container(
-                            decoration: BoxDecoration(
-                                shape: BoxShape.circle,
-                                color: mainTextColor,
-                                border: Border.all(
-                                  color: whiteColor,
-                                  width: 2,
-                                )),
-                            padding: EdgeInsets.all(
-                              10,
-                            ),
-                            child: Icon(
-                              FontAwesomeIcons.solidEdit,
-                            ),
+                          child: Icon(
+                            FontAwesomeIcons.solidEdit,
                           ),
-                        )
-                      ],
-                    ),
+                        ),
+                      )
+                    ],
                   ),
                 ),
               ),
-              SettingsTile(
-                title: 'Username',
-                titleTextStyle: GoogleFonts.openSans(
-                  fontSize: ScreenUtil().setSp(45),
-                  fontWeight: FontWeight.w600,
-                ),
-                leading: Icon(
-                  FontAwesomeIcons.penFancy,
-                  size: 18,
-                ),
+            ),
+            SettingsTile(
+              title: 'Username',
+              titleTextStyle: GoogleFonts.openSans(
+                fontSize: ScreenUtil().setSp(45),
+                fontWeight: FontWeight.w600,
               ),
-              SettingsTile(
-                title: 'Email',
-                titleTextStyle: GoogleFonts.openSans(
-                  fontSize: ScreenUtil().setSp(45),
-                  fontWeight: FontWeight.w600,
-                ),
-                leading: Icon(
-                  FontAwesomeIcons.solidEnvelope,
-                  size: 18,
-                ),
+              leading: Icon(
+                FontAwesomeIcons.penFancy,
+                size: 18,
               ),
-              SettingsTile(
-                title: 'Phone',
-                titleTextStyle: GoogleFonts.openSans(
-                  fontSize: ScreenUtil().setSp(45),
-                  fontWeight: FontWeight.w600,
-                ),
-                leading: Icon(
-                  FontAwesomeIcons.phone,
-                  size: 18,
-                ),
+            ),
+            SettingsTile(
+              title: 'Email',
+              titleTextStyle: GoogleFonts.openSans(
+                fontSize: ScreenUtil().setSp(45),
+                fontWeight: FontWeight.w600,
               ),
-              SettingsTile(
-                title: 'Change Password',
-                titleTextStyle: GoogleFonts.openSans(
-                  fontSize: ScreenUtil().setSp(45),
-                  fontWeight: FontWeight.w600,
-                ),
-                leading: Icon(
-                  FontAwesomeIcons.unlockAlt,
-                  size: 18,
-                ),
+              leading: Icon(
+                FontAwesomeIcons.solidEnvelope,
+                size: 18,
               ),
-              SettingsTile(
-                title: 'General',
-                titleTextStyle: TextStyle(
-                  fontFamily: 'Bambino',
-                  fontSize: ScreenUtil().setSp(40),
-                  fontWeight: FontWeight.w400,
-                ),
+            ),
+            SettingsTile(
+              title: 'Phone',
+              titleTextStyle: GoogleFonts.openSans(
+                fontSize: ScreenUtil().setSp(45),
+                fontWeight: FontWeight.w600,
               ),
-              SettingsTile(
-                title: 'Edit Listed Cars',
-                titleTextStyle: GoogleFonts.openSans(
-                  fontSize: ScreenUtil().setSp(45),
-                  fontWeight: FontWeight.w600,
-                ),
-                leading: Icon(
-                  FontAwesomeIcons.carAlt,
-                  size: 18,
-                ),
+              leading: Icon(
+                FontAwesomeIcons.phone,
+                size: 18,
               ),
-            ],
-          ),
+            ),
+            SettingsTile(
+              title: 'Change Password',
+              titleTextStyle: GoogleFonts.openSans(
+                fontSize: ScreenUtil().setSp(45),
+                fontWeight: FontWeight.w600,
+              ),
+              leading: Icon(
+                FontAwesomeIcons.unlockAlt,
+                size: 18,
+              ),
+            ),
+            SettingsTile(
+              title: 'General',
+              titleTextStyle: TextStyle(
+                fontFamily: 'Bambino',
+                fontSize: ScreenUtil().setSp(40),
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            SettingsTile(
+              title: 'Edit Listed Cars',
+              titleTextStyle: GoogleFonts.openSans(
+                fontSize: ScreenUtil().setSp(45),
+                fontWeight: FontWeight.w600,
+              ),
+              leading: Icon(
+                FontAwesomeIcons.carAlt,
+                size: 18,
+              ),
+            ),
+          ],
         ),
       ),
     );
