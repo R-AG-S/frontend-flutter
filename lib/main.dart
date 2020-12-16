6e94ff2d8b6ba164adadc44f3cb03488fc9d017f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 22:47:38 +0530

    app update

--- lib/main.dart
+++ lib/main.dart
@@ -22,6 +22,26 @@ class MainApp extends StatefulWidget {
 
 class _MainAppState extends State<MainApp> {
   @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: ${message['data']['message']}");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {});
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
