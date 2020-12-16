0570ba56ed576252741815c63969fc07893010cc
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 22:50:46 +0530

    app update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,4 @@
-import 'package:firebase_messaging/firebase_messaging.dart';
+// import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
