9adf3c00b6c2f4587b5048a4a3060ff36dfa879b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 12:58:00 +0530

    app update

--- lib/screens/editpage.dart
+++ lib/screens/editpage.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class EditPageScreen extends StatefulWidget {
+  @override
+  _EditPageScreenState createState() => _EditPageScreenState();
+}
+
+class _EditPageScreenState extends State<EditPageScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
