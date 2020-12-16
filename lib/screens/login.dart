6a7b64ddeffdddcab2d3702aa0d28eb5c6da1505
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-16 22:51:47 +0530

    app update

--- lib/screens/login.dart
+++ lib/screens/login.dart
@@ -1,4 +1,4 @@
-// import 'package:firebase_messaging/firebase_messaging.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_signin_button/flutter_signin_button.dart';
@@ -16,8 +16,29 @@ bool isWaiting = false;
 final TextEditingController _emailController = TextEditingController();
 final TextEditingController _passwordController = TextEditingController();
 final GlobalKey<ScaffoldState> _loginscaffoldKey = GlobalKey();
+final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
 
 class _LoginScreenState extends State<LoginScreen> {
+  @override
+  void initState() {
+    super.initState();
+    _firebaseMessaging.configure(
+      onMessage: (Map<String, dynamic> message) async {
+        print("onMessage: ${message['data']['message']}");
+      },
+      onLaunch: (Map<String, dynamic> message) async {
+        print("onLaunch: $message");
+      },
+      onResume: (Map<String, dynamic> message) async {
+        print("onResume: $message");
+      },
+    );
+    _firebaseMessaging.getToken().then((String token) {
+      assert(token != null);
+      setState(() {});
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     final height = MediaQuery.of(context).size.height;
