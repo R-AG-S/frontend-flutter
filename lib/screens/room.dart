d6179ad165c2dc299f8151c3234fe66f6293d595
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-17 19:59:45 +0530

    app update

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -23,30 +23,34 @@ class _RoomOptionsState extends State<RoomOptions> {
               'images/car_3.gif',
               fit: BoxFit.fitWidth,
             ),
-            Center(
-              child: Text(
-                'New Here?',
-                style: TextStyle(
-                  fontFamily: 'Bambino',
-                  fontSize: ScreenUtil().setSp(90),
-                  fontWeight: FontWeight.w700,
-                  color: whiteColor,
-                ),
-              ),
-            ),
-            Padding(
-              padding: EdgeInsets.all(0.0),
-              child: Center(
-                child: Text(
-                  '''Create or Join to Continue''',
-                  textAlign: TextAlign.center,
-                  style: GoogleFonts.raleway(
-                    fontSize: ScreenUtil().setSp(50),
-                    fontWeight: FontWeight.w400,
-                    color: mainTextColor,
+            Column(
+              children: [
+                Center(
+                  child: Text(
+                    'New Here?',
+                    style: TextStyle(
+                      fontFamily: 'Bambino',
+                      fontSize: ScreenUtil().setSp(90),
+                      fontWeight: FontWeight.w700,
+                      color: whiteColor,
+                    ),
                   ),
                 ),
-              ),
+                Padding(
+                  padding: EdgeInsets.all(0.0),
+                  child: Center(
+                    child: Text(
+                      '''Create or Join to Continue''',
+                      textAlign: TextAlign.center,
+                      style: GoogleFonts.raleway(
+                        fontSize: ScreenUtil().setSp(50),
+                        fontWeight: FontWeight.w400,
+                        color: mainTextColor,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
             ),
             Padding(
               padding: EdgeInsets.symmetric(
@@ -87,7 +91,7 @@ class _RoomOptionsState extends State<RoomOptions> {
                         onPressed: () {},
                         color: whiteColor,
                         child: Text(
-                          'join Room',
+                          'Join Room',
                           style: GoogleFonts.openSans(
                             fontSize: ScreenUtil().setSp(48),
                             color: darkFadeTextColor,
