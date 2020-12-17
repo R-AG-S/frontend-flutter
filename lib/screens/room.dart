e033d795e554760475617dddaeda11c887573512
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-17 19:51:12 +0530

    app update

--- lib/screens/room.dart
+++ lib/screens/room.dart
@@ -17,6 +17,30 @@ class _RoomOptionsState extends State<RoomOptions> {
               'images/car_3.gif',
               fit: BoxFit.fitWidth,
             ),
+            Center(
+              child: Text(
+                'Sign up to PayUp',
+                style: GoogleFonts.raleway(
+                  fontSize: ScreenUtil().setSp(65),
+                  fontWeight: FontWeight.w700,
+                  color: mainTextColor,
+                ),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.all(8.0),
+              child: Center(
+                child: Text(
+                  '''Car Pool with friends. It's always fun!!!''',
+                  textAlign: TextAlign.center,
+                  style: GoogleFonts.raleway(
+                    fontSize: ScreenUtil().setSp(40),
+                    fontWeight: FontWeight.w400,
+                    color: mainTextColor,
+                  ),
+                ),
+              ),
+            ),
           ],
         ),
       ),
