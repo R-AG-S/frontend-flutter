6ababfcad0a06992ff54bbeca2c320937774b51a
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-17 23:48:17 +0530

    app update

--- lib/screens/createRoom.dart
+++ lib/screens/createRoom.dart
@@ -8,8 +8,97 @@ class CreateRoomScreen extends StatefulWidget {
 class _CreateRoomScreenState extends State<CreateRoomScreen> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(
-      child: Scaffold(),
+    return MaterialApp(
+      builder: DevicePreview.appBuilder,
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        brightness: Brightness.light,
+        iconTheme: IconThemeData(color: darkFadeTextColor),
+        primaryTextTheme: Typography.material2018().black,
+        textTheme: Typography.material2018().black,
+      ),
+      darkTheme: ThemeData(
+        brightness: Brightness.dark,
+        canvasColor: mainTextColor,
+        primaryTextTheme: Typography.material2018().white,
+        textTheme: Typography.material2018().white,
+      ),
+      home: SafeArea(
+        child: Scaffold(
+          appBar: AppBar(
+            elevation: 0,
+            title: Text(
+              'Edit ' + widget.name,
+              style: TextStyle(
+                fontFamily: 'Bambino',
+                letterSpacing: 0.5,
+                fontSize: 22,
+                fontWeight: FontWeight.w400,
+              ),
+            ),
+            backgroundColor: Colors.transparent,
+          ),
+          body: Container(
+            padding: EdgeInsets.symmetric(
+              horizontal: 25,
+            ),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 8,
+                  ),
+                  child: TextField(
+                    // style: TextStyle(color: mainTextColor),
+                    // cursorColor: mainTextColor,
+                    controller: _editController,
+                    keyboardType: widget.inputType,
+                    maxLength: 50,
+                    decoration: InputDecoration(
+                      labelText: widget.value,
+                      border: OutlineInputBorder(
+                        borderSide: BorderSide.none,
+                        borderRadius: BorderRadius.all(
+                          Radius.circular(5.0),
+                        ),
+                      ),
+                      contentPadding: EdgeInsets.symmetric(
+                        vertical: 10,
+                        horizontal: 15,
+                      ),
+                      counterText: '',
+                    ),
+                  ),
+                ),
+                Padding(
+                  padding: EdgeInsets.symmetric(
+                    vertical: 24,
+                  ),
+                  child: ButtonTheme(
+                    buttonColor: Colors.greenAccent[400],
+                    focusColor: Colors.greenAccent[400],
+                    minWidth: MediaQuery.of(context).size.width,
+                    height: 50.0,
+                    child: RaisedButton(
+                      color: Colors.greenAccent[400],
+                      child: Text(
+                        'Save',
+                        style: GoogleFonts.openSans(
+                          fontSize: 18,
+                          color: whiteColor,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      onPressed: () {},
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
