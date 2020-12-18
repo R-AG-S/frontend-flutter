94f650295a9ed61c67c3c8056ebbfd8e173bbedb
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-18 18:24:16 +0530

    app update

--- lib/widgets/qr.dart
+++ lib/widgets/qr.dart
@@ -1,6 +1,7 @@
 import 'dart:async';
 import 'dart:ui' as ui;
 
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
