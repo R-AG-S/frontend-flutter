7f9113f69ef585d58342ce7387636373cbea8b39
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 00:17:35 +0530

    app update

--- lib/integration/createRoom.dart
+++ lib/integration/createRoom.dart
@@ -2,4 +2,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:payup/integration/refresh.dart';
 
-createRoom() {}
+createRoom() {
+  try {
+    final authKey = await refreshToken();
+  } catch (e) {}
+}
