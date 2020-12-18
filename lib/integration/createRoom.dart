0728a69bc8e682adcf24ad797b9b8e49a8a443dc
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 00:18:57 +0530

    app update

--- lib/integration/createRoom.dart
+++ lib/integration/createRoom.dart
@@ -5,5 +5,24 @@ import 'package:payup/integration/refresh.dart';
 createRoom() async {
   try {
     final authKey = await refreshToken();
+    final response = await http.post(
+      'https://payup-backend.herokuapp.com/carpool/create_room',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+      body: json.encode(
+        <String, String>{
+          "room_name": _nameController.text,
+          "details": _detailsController.text,
+          "petrol_price": _fuelController.text
+        },
+      ),
+    );
+    print(
+      jsonDecode(response.body),
+    );
+    final qrCode = jsonDecode(response.body)['ROOM_ID'];
   } catch (e) {}
 }
