27c151d59b264053b441d13e795f8e0f3df2f83c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-18 22:17:02 +0530

    app update

---
+++ lib/integration/carDetails.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/integration/refresh.dart';
+
+getUserData() async {
+  try {
+    final authKey = await refreshToken();
+    final refreshBody = await http.get(
+      'https://payup-backend.herokuapp.com/users/get_user_data/',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+    );
+    print(jsonDecode(refreshBody.body));
+  } catch (e) {}
+}
