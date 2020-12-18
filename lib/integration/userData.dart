25a71bc507850e6e8f65a21e98e99de430576fec
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-18 21:18:50 +0530

    app update

---
+++ lib/integration/userData.dart
@@ -0,0 +1,22 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+
+Future<String> refreshToken() async {
+  final SharedPreferences prefs = await SharedPreferences.getInstance();
+  try {
+    final refreshToken = prefs.getString('refreshToken');
+    print(refreshToken);
+    final refreshBody = await http.get(
+      'https://payup-backend.herokuapp.com/users/refresh_token/',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+      },
+    );
+    print(jsonDecode(refreshBody.body)['access_token']);
+    return (jsonDecode(refreshBody.body)['access_token']);
+  } catch (e) {
+    return 'Error';
+  }
+}
