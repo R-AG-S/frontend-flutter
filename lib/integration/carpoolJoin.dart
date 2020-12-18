767b4dfbdfba125087b0addd7ab5bb9a5433c95f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-18 22:24:34 +0530

    app update

--- lib/integration/carpoolJoin.dart
+++ lib/integration/carpoolJoin.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:payup/integration/refresh.dart';
 
-getUserData() async {
+joinRoom() async {
   try {
     final authKey = await refreshToken();
     final refreshBody = await http.get(
