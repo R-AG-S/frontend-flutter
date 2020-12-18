52bc20e74d44866df0afbd650425ae1fb7cdd556
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 00:42:16 +0530

    app update

--- lib/backend/carDetails.dart
+++ lib/backend/carDetails.dart
@@ -1,6 +1,6 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:payup/integration/refresh.dart';
+import 'package:payup/backend/refresh.dart';
 
 getCarDetails() async {
   try {
