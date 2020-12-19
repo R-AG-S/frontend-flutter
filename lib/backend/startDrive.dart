1a72963c28d41048832567655362c3156cb62782
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 01:02:34 +0530

    app update

---
+++ lib/backend/startDrive.dart
@@ -0,0 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/backend/refresh.dart';
+
+setCarDetails(String carModel, String milege) async {
+  try {
+    final authKey = await refreshToken();
+    final setCarDetails = await http.post(
+      'https://payup-backend.herokuapp.com/carpool/join_room',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+      body: json.encode(
+        <String, String>{"car_model": carModel, "mileage": milege},
+      ),
+    );
+    print(jsonDecode(setCarDetails.body));
+  } catch (e) {}
+}
+
+/active/start_drive
