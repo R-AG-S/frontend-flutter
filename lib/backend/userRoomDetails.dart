f38009a02d078d7851b187cbd3bd446b3e100a5d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 17:58:23 +0530

    app update

---
+++ lib/backend/userRoomDetails.dart
@@ -0,0 +1,18 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/backend/refresh.dart';
+
+userRoomId() async {
+  try {
+    final authKey = await refreshToken();
+    final userRoomId = await http.get(
+      'https://payup-backend.herokuapp.com/carpool/user_rooms/id',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+    );
+    print(jsonDecode(userRoomId.body));
+  } catch (e) {}
+}
