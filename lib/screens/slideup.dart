f78f18eba39154ccf5650c9e558a8a81327c9806
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 00:27:28 +0530

    app update

--- lib/screens/slideup.dart
+++ lib/screens/slideup.dart
@@ -211,63 +211,67 @@ class _SlideUpPanelState extends State<SlideUpPanel> {
                   vertical: 10,
                   horizontal: 15,
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  mainAxisSize: MainAxisSize.max,
-                  children: [
-                    Text(
-                      'Participants',
-                      textAlign: TextAlign.left,
-                      style: GoogleFonts.openSans(
-                        fontSize: 14,
-                        fontWeight: FontWeight.w400,
+                child: Padding(
+                  padding: EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    mainAxisSize: MainAxisSize.max,
+                    children: [
+                      Text(
+                        'Participants',
+                        textAlign: TextAlign.left,
+                        style: GoogleFonts.openSans(
+                          fontSize: 14,
+                          fontWeight: FontWeight.w400,
+                          color: darkFadeTextColor,
+                        ),
+                      ),
+                      Divider(
+                        thickness: 0.2,
                         color: darkFadeTextColor,
                       ),
-                    ),
-                    Divider(
-                      thickness: 0.2,
-                      color: darkFadeTextColor,
-                    ),
-                    ListView.builder(
-                      physics: NeverScrollableScrollPhysics(),
-                      shrinkWrap: true,
-                      itemCount: iconData.length,
-                      itemBuilder: (BuildContext context, int index) {
-                        return Column(
-                          children: [
-                            Row(
-                              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                              children: [
-                                FadeInImage.assetNetwork(
-                                  fadeOutDuration: Duration(seconds: 1),
-                                  image: iconData[index],
-                                  placeholder: 'images/place.png',
-                                  height: 45,
-                                ),
-                                Text(
-                                  'Demo User $index',
-                                  style: GoogleFonts.openSans(
-                                    fontSize: 14,
-                                    fontWeight: FontWeight.w400,
-                                    color: darkFadeTextColor,
+                      ListView.builder(
+                        physics: NeverScrollableScrollPhysics(),
+                        shrinkWrap: true,
+                        itemCount: iconData.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return Column(
+                            children: [
+                              Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: [
+                                  FadeInImage.assetNetwork(
+                                    fadeOutDuration: Duration(seconds: 1),
+                                    image: iconData[index],
+                                    placeholder: 'images/place.png',
+                                    height: 45,
                                   ),
-                                ),
-                                Icon(
-                                  FontAwesomeIcons.solidCircle,
-                                  color: index % 2 == 0
-                                      ? Colors.green
-                                      : Colors.red,
-                                  size: 10,
-                                )
-                              ],
-                            ),
-                            Divider()
-                          ],
-                        );
-                      },
-                    ),
-                  ],
+                                  Text(
+                                    'Demo User $index',
+                                    style: GoogleFonts.openSans(
+                                      fontSize: 14,
+                                      fontWeight: FontWeight.w400,
+                                      color: darkFadeTextColor,
+                                    ),
+                                  ),
+                                  Icon(
+                                    FontAwesomeIcons.solidCircle,
+                                    color: index % 2 == 0
+                                        ? Colors.green
+                                        : Colors.red,
+                                    size: 10,
+                                  )
+                                ],
+                              ),
+                              Divider()
+                            ],
+                          );
+                        },
+                      ),
+                    ],
+                  ),
                 ),
               )
             ],
