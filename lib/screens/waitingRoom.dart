45b31476f20fbf8f5ffe5177d26f75f14060345b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 21:29:47 +0530

    app update

--- lib/screens/waitingRoom.dart
+++ lib/screens/waitingRoom.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:payup/backend/userRoomId.dart';
+import 'package:payup/screens/home.dart';
 
 class WaitingScreen extends StatefulWidget {
   @override
@@ -6,6 +8,19 @@ class WaitingScreen extends StatefulWidget {
 }
 
 class _WaitingScreenState extends State<WaitingScreen> {
+  @override
+  void initState() {
+    super.initState();
+    final userIdList = userRoomId();
+    if (userIdList.length > 0) {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (context) => HomeScreen(),
+        ),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
