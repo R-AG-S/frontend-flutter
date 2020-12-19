c2fee723bcd2f654fd96ba0ef1ea75bf7380038a
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-19 17:57:13 +0530

    app update

--- lib/backend/userRoomId.dart
+++ lib/backend/userRoomId.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:payup/backend/refresh.dart';
 
-getUserData() async {
+userRoomId() async {
   try {
     final authKey = await refreshToken();
     final userDetails = await http.get(
