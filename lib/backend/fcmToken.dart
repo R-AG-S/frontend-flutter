39c3aa7ea188234009ba016d7c7d944b3f9adc94
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 19:13:14 +0530

    app update

--- lib/backend/fcmToken.dart
+++ lib/backend/fcmToken.dart
@@ -1,12 +1,14 @@
 import 'package:firebase_messaging/firebase_messaging.dart';
 
-Future<String> getFCMToken() {
+Future<String> getFCMToken() async {
   final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
   _firebaseMessaging.configure();
-  _firebaseMessaging.getToken().then((String token) {
+  String tokenKey = '';
+  await _firebaseMessaging.getToken().then((String token) {
     assert(token != null);
     final _homeScreenText = "Push Messaging token: $token";
     print(_homeScreenText);
-    return (token);
+    tokenKey = token;
   });
+  return (tokenKey);
 }
