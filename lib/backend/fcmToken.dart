63397ab30cca4776b7f8e2aac2605e348b413caa
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 19:05:20 +0530

    app update

---
+++ lib/backend/fcmToken.dart
@@ -0,0 +1,11 @@
+import 'package:firebase_messaging/firebase_messaging.dart';
+
+getFCMToken() {
+  final FirebaseMessaging _firebaseMessaging = FirebaseMessaging();
+  _firebaseMessaging.configure();
+  _firebaseMessaging.getToken().then((String token) {
+    assert(token != null);
+    final _homeScreenText = "Push Messaging token: $token";
+    print(_homeScreenText);
+  });
+}
