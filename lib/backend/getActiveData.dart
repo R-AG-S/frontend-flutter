01d80700f8b50a75d52d4eb106d8ed65d0ab3d5d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 22:25:53 +0530

    app update

---
+++ lib/backend/getActiveData.dart
@@ -0,0 +1,24 @@
+/active/get_drive_data
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/backend/refresh.dart';
+
+getUserData(String userId) async {
+  try {
+    final authKey = await refreshToken();
+    final userDetails = await http.get(
+      'https://payup-backend.herokuapp.com/users/get_any_user_display_data/$userId',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+    );
+    return ({
+      'name': jsonDecode(userDetails.body)['displayname'],
+      'dp': jsonDecode(userDetails.body)['displaypic'],
+    });
+  } catch (e) {
+    return {};
+  }
+}
