e9d335e29995ce405450b0d1914fb027471916ef
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 20:42:43 +0530

    app update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -149,6 +149,7 @@ class _HomeScreenState extends State<HomeScreen> {
                       membersList: roomDetails[counter]['data']['members'],
                       petrol: roomDetails[counter]['data']['petrol_price'],
                       roomName: roomDetails[counter]['data']['room_name'],
+                      desc: roomDetails[counter]['data']['owner'],
                     )
                   : Container(),
               body: isReady
