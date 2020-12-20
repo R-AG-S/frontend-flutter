19ec484ca90fbc06ed06a9c307f6985befb64f74
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 22:26:18 +0530

    app update

--- lib/backend/getActiveData.dart
+++ lib/backend/getActiveData.dart
@@ -1,24 +1,23 @@
-/active/get_drive_data
 import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:payup/backend/refresh.dart';
 
-getUserData(String userId) async {
+leaveDrive(String roomId, double lat, double long) async {
   try {
     final authKey = await refreshToken();
-    final userDetails = await http.get(
-      'https://payup-backend.herokuapp.com/users/get_any_user_display_data/$userId',
+    final leaveDrive = await http.post(
+      'https://payup-backend.herokuapp.com/active/leave_drive',
       headers: <String, String>{
         'Content-type': 'application/json',
         'Accept': 'application/json',
         "Authorization": authKey
       },
+      body: json.encode(
+        <String, dynamic>{"room_id": roomId, "lat": lat, "lng": long},
+      ),
     );
-    return ({
-      'name': jsonDecode(userDetails.body)['displayname'],
-      'dp': jsonDecode(userDetails.body)['displaypic'],
-    });
+    return (leaveDrive.statusCode);
   } catch (e) {
-    return {};
+    return 400;
   }
 }
