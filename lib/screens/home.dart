97c860fa3869ece4c82c03ea0d6231c8c9013f8f
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 21:57:26 +0530

    app update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -486,7 +486,7 @@ class _HomeScreenState extends State<HomeScreen> {
                                             backgroundColor: redColor,
                                             title: "Error",
                                             message:
-                                                'USER_ALREADY_JOINED_ACTIVE_SESSION',
+                                                'ROOM_ALREADY_HAS_ACTIVE_SESSION',
                                             duration: Duration(seconds: 3),
                                           )..show(context);
                                         }
