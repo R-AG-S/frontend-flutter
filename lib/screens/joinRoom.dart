c5dda9e005fa847bd508c64f5dc9c968fcc02a5b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 12:58:00 +0530

    app update

--- lib/screens/joinRoom.dart
+++ lib/screens/joinRoom.dart
@@ -8,6 +8,8 @@ class QRScanner extends StatefulWidget {
   _QRScannerState createState() => _QRScannerState();
 }
 
+String qrCodeResult = "Not Yet Scanned";
+
 class _QRScannerState extends State<QRScanner> {
   @override
   Widget build(BuildContext context) {
@@ -33,63 +35,60 @@ class _QRScannerState extends State<QRScanner> {
       ),
       home: SafeArea(
         child: Scaffold(
-      appBar: AppBar(
-        title: Text("Scanner"),
-        centerTitle: true,
-      ),
-      body: Container(
-        padding: EdgeInsets.all(20.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: <Widget>[
-            Text(
-              "Result",
-              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
-              textAlign: TextAlign.center,
+          appBar: AppBar(
+            title: Text("Scanner"),
+            centerTitle: true,
+          ),
+          body: Container(
+            padding: EdgeInsets.all(20.0),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.stretch,
+              children: <Widget>[
+                Text(
+                  "Result",
+                  style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+                  textAlign: TextAlign.center,
+                ),
+                Text(
+                  qrCodeResult,
+                  style: TextStyle(
+                    fontSize: 20.0,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+                SizedBox(
+                  height: 20.0,
+                ),
+                FlatButton(
+                  padding: EdgeInsets.all(15.0),
+                  onPressed: () async {
+                    String codeSanner =
+                        await BarcodeScanner.scan(); //barcode scnner
+                    setState(() {
+                      qrCodeResult = codeSanner;
+                    });
+
+                    // try{
+                    //   BarcodeScanner.scan()    this method is used to scan the QR code
+                    // }catch (e){
+                    //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
+                    //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
+                    // }
+                  },
+                  child: Text(
+                    "Open Scanner",
+                    style: TextStyle(
+                        color: Colors.blue, fontWeight: FontWeight.bold),
+                  ),
+                  shape: RoundedRectangleBorder(
+                      side: BorderSide(color: Colors.blue, width: 3.0),
+                      borderRadius: BorderRadius.circular(20.0)),
+                )
+              ],
             ),
-            Text(
-              qrCodeResult,
-              style: TextStyle(
-                fontSize: 20.0,
-              ),
-              textAlign: TextAlign.center,
-            ),
-            SizedBox(
-              height: 20.0,
-            ),
-            FlatButton(
-              padding: EdgeInsets.all(15.0),
-              onPressed: () async {
-
-
-                String codeSanner = await BarcodeScanner.scan();    //barcode scnner
-                setState(() {
-                  qrCodeResult = codeSanner;
-                });
-
-                // try{
-                //   BarcodeScanner.scan()    this method is used to scan the QR code
-                // }catch (e){
-                //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
-                //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
-                // }
-
-
-              },
-              child: Text(
-                "Open Scanner",
-                style:
-                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
-              ),
-              shape: RoundedRectangleBorder(
-                  side: BorderSide(color: Colors.blue, width: 3.0),
-                  borderRadius: BorderRadius.circular(20.0)),
-            )
-          ],
+          ),
         ),
-      ),
-    );,
       ),
     );
   }
