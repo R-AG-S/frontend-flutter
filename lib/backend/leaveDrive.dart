5332286d7bd849e42b36713b16a2b6c086ae0b95
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 21:45:23 +0530

    app update

---
+++ lib/backend/leaveDrive.dart
@@ -0,0 +1,21 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/backend/refresh.dart';
+
+joinRoom(String roomId, double lat, double long) async {
+  try {
+    final authKey = await refreshToken();
+    final joinRoom = await http.post(
+      'https://payup-backend.herokuapp.com/active/leave_drive',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+      body: json.encode(
+        <String, String>{"room_id": roomId},
+      ),
+    );
+    return (joinRoom.statusCode);
+  } catch (e) {}
+}
