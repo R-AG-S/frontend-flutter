8f9f4a4fdd7b60f3bb52a493cac598caabe0f3a7
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-20 12:57:13 +0530

    app update

--- lib/screens/joinRoom.dart
+++ lib/screens/joinRoom.dart
@@ -32,7 +32,64 @@ class _QRScannerState extends State<QRScanner> {
         textTheme: Typography.material2018().white,
       ),
       home: SafeArea(
-        child: Scaffold(),
+        child: Scaffold(
+      appBar: AppBar(
+        title: Text("Scanner"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(20.0),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Result",
+              style: TextStyle(fontSize: 25.0, fontWeight: FontWeight.bold),
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              qrCodeResult,
+              style: TextStyle(
+                fontSize: 20.0,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            SizedBox(
+              height: 20.0,
+            ),
+            FlatButton(
+              padding: EdgeInsets.all(15.0),
+              onPressed: () async {
+
+
+                String codeSanner = await BarcodeScanner.scan();    //barcode scnner
+                setState(() {
+                  qrCodeResult = codeSanner;
+                });
+
+                // try{
+                //   BarcodeScanner.scan()    this method is used to scan the QR code
+                // }catch (e){
+                //   BarcodeScanner.CameraAccessDenied;   we can print that user has denied for the permisions
+                //   BarcodeScanner.UserCanceled;   we can print on the page that user has cancelled
+                // }
+
+
+              },
+              child: Text(
+                "Open Scanner",
+                style:
+                    TextStyle(color: Colors.blue, fontWeight: FontWeight.bold),
+              ),
+              shape: RoundedRectangleBorder(
+                  side: BorderSide(color: Colors.blue, width: 3.0),
+                  borderRadius: BorderRadius.circular(20.0)),
+            )
+          ],
+        ),
+      ),
+    );,
       ),
     );
   }
