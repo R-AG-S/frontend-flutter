a235306c979b4bfed7175e0a05c8ae62e55dbc4c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 15:01:51 +0530

    app update

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -108,7 +108,29 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   _getLocation() async {
-    await Geolocator.checkPermission();
+    bool serviceEnabled;
+    LocationPermission permission;
+
+    serviceEnabled = await Geolocator.isLocationServiceEnabled();
+    if (!serviceEnabled) {
+      return Future.error('Location services are disabled.');
+    }
+
+    permission = await Geolocator.checkPermission();
+    if (permission == LocationPermission.deniedForever) {
+      return Future.error(
+          'Location permissions are permantly denied, we cannot request permissions.');
+    }
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission != LocationPermission.whileInUse &&
+          permission != LocationPermission.always) {
+        return Future.error(
+            'Location permissions are denied (actual value: $permission).');
+      }
+    }
+
     Position position = await Geolocator.getCurrentPosition(
       desiredAccuracy: LocationAccuracy.bestForNavigation,
     );
