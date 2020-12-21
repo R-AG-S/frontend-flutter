528119668dde57ecb993caf28037e23256d60ce5
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 11:32:26 +0530

    app update

--- lib/backend/loginUser.dart
+++ lib/backend/loginUser.dart
@@ -1,3 +1,27 @@
 import 'dart:convert';
 import 'package:http/http.dart' as http;
-import 'package:payup/backend/refresh.dart';
+
+Future<String> loginUser(String email, String password, String fcm) async {
+  try {
+    final response = await http.post(
+      'https://payup-backend.herokuapp.com/users/login/',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+      },
+      body: json.encode(
+        <String, String>{
+          "email": _emailController.text,
+          "password": _passwordController.text,
+          "device_registeration_token": fcmToken
+        },
+      ),
+    );
+    print(
+      jsonDecode(response.body),
+    );
+    return (jsonDecode(refreshBody.body)['access_token']);
+  } catch (e) {
+    return 'Error';
+  }
+}
