ad6cb0e4512bee7b9ac84fdbe32a5b9a03d2e09b
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 21:46:53 +0530

    app update

--- lib/backend/getCarDetails.dart
+++ lib/backend/getCarDetails.dart
@@ -2,7 +2,7 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'package:payup/backend/refresh.dart';
 
-getUserData(String userId) async {
+getUserData() async {
   try {
     final authKey = await refreshToken();
     final userDetails = await http.get(
