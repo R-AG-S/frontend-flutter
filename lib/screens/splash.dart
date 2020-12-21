74126e42952090d1c63973737df8582cd69ccc2d
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 13:29:00 +0530

    app update

--- lib/screens/splash.dart
+++ lib/screens/splash.dart
@@ -2,7 +2,10 @@ import 'dart:async';
 
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:payup/backend/refresh.dart';
+import 'package:payup/screens/waitingRoom.dart';
 import 'package:payup/utilities/constants.dart';
+import 'package:shared_preferences/shared_preferences.dart';
 
 class SplashScreen extends StatefulWidget {
   @override
@@ -10,6 +13,37 @@ class SplashScreen extends StatefulWidget {
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Firebase.initializeApp().whenComplete(() async {
+      print('Connected');
+      final isReady = await checkReady();
+      Timer(Duration(seconds: 3), () {
+        if (isReady) {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => WaitingScreen(),
+            ),
+          );
+        } else {
+          Navigator.pushNamed(context, 'login');
+        }
+      });
+      setState(() {});
+    });
+  }
+
+  Future<bool> checkReady() async {
+    final response = await refreshToken();
+    if (response == 'Error') {
+      return false;
+    } else {
+      return true;
+    }
+  }
+
   @override
   void initState() {
     super.initState();
