5c901ae27cda957af199e36c045691fd2f45cdda
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 11:47:55 +0530

    app update

--- lib/backend/setNameDp.dart
+++ lib/backend/setNameDp.dart
@@ -1,1 +1,23 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+import 'package:payup/backend/refresh.dart';
+
+joinRoom(String roomId) async {
+  try {
+    final authKey = await refreshToken();
+    final joinRoom = await http.post(
+      'https://payup-backend.herokuapp.com/carpool/join_room',
+      headers: <String, String>{
+        'Content-type': 'application/json',
+        'Accept': 'application/json',
+        "Authorization": authKey
+      },
+      body: json.encode(
+        <String, String>{"room_id": roomId},
+      ),
+    );
+    return (joinRoom.statusCode);
+  } catch (e) {}
+}
+
 /users/set_name_and_pic/
