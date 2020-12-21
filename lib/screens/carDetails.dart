c56d605287d2110c66d09611c1638d7bd9f39f9c
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 20:06:25 +0530

    app update

--- lib/screens/carDetails.dart
+++ lib/screens/carDetails.dart
@@ -3,6 +3,7 @@ import 'package:flushbar/flushbar.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:payup/backend/setCarDetails.dart';
 import 'package:payup/screens/room.dart';
 import 'package:payup/utilities/constants.dart';
 import 'package:payup/widgets/textform.dart';
@@ -181,6 +182,9 @@ class _CarDetailsScreenState extends State<CarDetailsScreen> {
                                                           .text.isNotEmpty &&
                                                       _milegeController
                                                           .text.isNotEmpty) {
+                                                    setCarDetails(
+                                                        _carController.text,
+                                                        _milegeController.text);
                                                   } else {
                                                     Flushbar(
                                                       backgroundColor: redColor,
