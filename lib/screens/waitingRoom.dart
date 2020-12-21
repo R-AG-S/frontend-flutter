7eda3e2b5787a3936a01a81dc1ccca70ba247fbb
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 13:35:42 +0530

    app update

--- lib/screens/waitingRoom.dart
+++ lib/screens/waitingRoom.dart
@@ -1,5 +1,6 @@
 import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:payup/backend/userRoomId.dart';
 import 'package:payup/screens/home.dart';
 import 'package:payup/screens/room.dart';
@@ -36,25 +37,31 @@ class _WaitingScreenState extends State<WaitingScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      builder: DevicePreview.appBuilder,
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: white,
-        accentColor: black,
-      ),
-      darkTheme: ThemeData(
-        primarySwatch: black,
-        accentColor: white,
-        brightness: Brightness.dark,
-        canvasColor: mainTextColor,
-        primaryTextTheme: Typography.material2018().white,
-        textTheme: Typography.material2018().white,
-      ),
-      home: SafeArea(
-        child: Scaffold(
-          body: Center(
-            child: CircularProgressIndicator(),
+    return WillPopScope(
+      onWillPop: () async {
+        await SystemChannels.platform.invokeMethod<void>('SystemNavigator.pop');
+        return false;
+      },
+      child: MaterialApp(
+        builder: DevicePreview.appBuilder,
+        debugShowCheckedModeBanner: false,
+        theme: ThemeData(
+          primarySwatch: white,
+          accentColor: black,
+        ),
+        darkTheme: ThemeData(
+          primarySwatch: black,
+          accentColor: white,
+          brightness: Brightness.dark,
+          canvasColor: mainTextColor,
+          primaryTextTheme: Typography.material2018().white,
+          textTheme: Typography.material2018().white,
+        ),
+        home: SafeArea(
+          child: Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
           ),
         ),
       ),
