fb17c81ea04115f8d3b4ec566d87c5e89c9518f1
rahulrmsh <rahulramesh13.rr@gmail.com>
2020-12-21 19:13:00 +0530

    app update

--- lib/screens/report.dart
+++ lib/screens/report.dart
@@ -1,1 +1,13 @@
 import 'package:flutter/material.dart';
+
+class FinalReport extends StatefulWidget {
+  @override
+  _FinalReportState createState() => _FinalReportState();
+}
+
+class _FinalReportState extends State<FinalReport> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
